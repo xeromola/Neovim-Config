a1b53ed50130117d46ecf5eeca20ef2a2bffc6ee
Ujjawal Srivastava <srivastava.ujjawal011@gmail.com>
2023-02-24 19:51:43 +0530

    LSP/AutoPairs/Harpoon etc added

--- init.lua
+++ init.lua
@@ -1,2 +1,1 @@
 require("xeromola")
---vim.cmd.colorscheme = "material"
--- lua/xeromola/packer.lua
+++ lua/xeromola/packer.lua
@@ -17,6 +17,39 @@ return require('packer').startup(function(use)
   -- Git signs
   use 'lewis6991/gitsigns.nvim'
 
+  -- Harpoon
+  use 'ThePrimeagen/harpoon'
+
+  -- LSP
+  use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v1.x',
+      requires = {
+          -- LSP Support
+          {'neovim/nvim-lspconfig'},             -- Required
+          {'williamboman/mason.nvim'},           -- Optional
+          {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+          -- Autocompletion
+          {'hrsh7th/nvim-cmp'},         -- Required
+          {'hrsh7th/cmp-nvim-lsp'},     -- Required
+          {'hrsh7th/cmp-buffer'},       -- Optional
+          {'hrsh7th/cmp-path'},         -- Optional
+          {'saadparwaiz1/cmp_luasnip'}, -- Optional
+          {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+          -- Snippets
+          {'L3MON4D3/LuaSnip'},             -- Required
+          {'rafamadriz/friendly-snippets'}, -- Optional
+      }
+  }
+
+  -- Auto Pairing
+  use {
+      "windwp/nvim-autopairs",
+      config = function() require("nvim-autopairs").setup {} end
+  }
+
   -- Color Theme 
   use 'marko-cerovac/material.nvim'
   use 'rose-pine/neovim'
